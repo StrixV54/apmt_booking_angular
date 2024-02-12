f74cdedfb8e28240acd3e579d582c12eea87728d
shrikantv5 <64365738+StrixV54@users.noreply.github.com>
2024-02-12 18:43:42 +0530

    added testcases for home

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,10 +8,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
-    
+      imports: [HomeComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,34 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show app name "HEALTHCARE ANGULAR"', () => {
+    const h1 = fixture.debugElement.query(By.css('header > h1'));
+    expect(h1.nativeElement.textContent).toBe('HEALTHCARE ANGULAR');
+  });
+
+  it('should show list of professionals that are stored in filterlist when no speciality selected', () => {
+    const selectorInput = fixture.debugElement.query(By.css('header select'));
+    selectorInput.triggerEventHandler('change', {
+      target: { value: 'None' },
+    });
+    fixture.detectChanges();
+
+    const liFromComp = component.filterList.length;
+    const liFromHTML = fixture.debugElement.queryAll(By.css('.card-box > li'));
+    expect(liFromHTML.length).toBe(liFromComp);
+  });
+
+  it('should show list of professionals that are stored in filterlist when changing speciality to "Cardiologist"', () => {
+    const selectorInput = fixture.debugElement.query(By.css('header select'));
+    selectorInput.triggerEventHandler('change', {
+      target: { value: 'Cardiologist' },
+    });
+    fixture.detectChanges();
+
+    const liFromComp = component.filterList.length;
+    const liFromHTML = fixture.debugElement.queryAll(By.css('.card-box > li'));
+    expect(liFromHTML.length).toBe(liFromComp);
   });
 });
